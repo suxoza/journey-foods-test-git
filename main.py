066da5e7367cfb0bbb1c533f3728cfe98a51efc4
mixeil <suxoza@gmail.com>
2022-03-09 04:10:58 +0400

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+api = Blueprint("app", __name__, url_prefix = '/')
+api2 = Blueprint("app2", __name__, url_prefix = '/app2')
+
+
+
+@api.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+@api2.route('/')
+def hello2():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World 2!'
+
+
+app.register_blueprint(api)
+app.register_blueprint(api2)
+
+
+if __name__ == "__main__":
+    # Dev only: run "python main.py" and open http://localhost:8080
+    app.run(host="localhost", port=8080, debug=True)
+
